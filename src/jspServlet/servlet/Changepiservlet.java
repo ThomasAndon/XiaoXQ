f5d0bfcc68d1395561554fdb4c0a3ca550422830
Thomas Andon <ThomasAndon@users.noreply.github.com>
2020-10-20 10:49:06 +0800

    增加了用户修改个人信息和密码后的弹窗提示功能。

--- src/jspServlet/servlet/Changepiservlet.java
+++ src/jspServlet/servlet/Changepiservlet.java
@@ -32,6 +32,6 @@ public class Changepiservlet extends HttpServlet {
             req.getSession().setAttribute("message", "Info Changed SUCCESSFULLY");
         }
 
-        resp.sendRedirect("./profile.jsp");
+        resp.sendRedirect(req.getContextPath()+"/profile.jsp");
     }
 }
